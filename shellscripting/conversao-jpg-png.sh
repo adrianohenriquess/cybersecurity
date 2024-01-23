b866de24eba06958260ada4dde675bf8d1c711f7
adrianohenriquess <adriano.henriquesds@gmail.com>
2024-01-23 18:19:08 -0300

    Convertendo todos os arquivos de um diretorio e enviando para outro, verfificando o status de saida igual a 0 para imprimir mensagem de sucesso, redirecionando o erro para um arquivo de texto

--- shellscripting/conversao-jpg-png.sh
+++ shellscripting/conversao-jpg-png.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
-CAMINHO_IMAGENS=imagens-livros
 
-for imagem in $@
+converte_imagem() {
+cd imagens-livros/
+
+#se nao existe o diretorio png, crie
+if [ ! -d png ]
+then 
+	mkdir png
+fi
+
+for imagem in *.jpg
 do 
-	convert $CAMINHO_IMAGENS/$imagem.jpg $CAMINHO_IMAGENS/$imagem.png
+        local imagem_sem_extensao=$(ls $imagem | awk -F. '{ print $1 }')
+	convert $imagem_sem_extensao.jpg png/$imagem_sem_extensao.png
 done
+}
+
+#o numero 2 pega o erro e pode ser direcionado para um arquivo erros_conversao.txt
+converte_imagem 2>erros_conversao.txt
+#$? pega o status de saida da função
+if [ $? -eq 0 ] 
+then
+	echo "Conversão realizada com sucesso!"
+else
+	echo "Houve uma falha no processo de conversão."
+fi
+
